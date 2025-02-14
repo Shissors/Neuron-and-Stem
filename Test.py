1adf801efab56c3523b9dac599cd65cb5e131ecf
shishir <shishirshreyasnair@gmail.com>
2025-02-14 16:42:20 +0000

    ok

--- Test.py
+++ Test.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
-
+import scipy.stats as stats
 
 
 df_stem = pd.read_csv("GSE60905_series_matrix.txt", comment="!", sep="\t", index_col=0)
@@ -93,16 +93,68 @@ df_merged_neuron=df_merged_neuron.drop(["PC1","PC2"], axis=1)
 
 
 df_combined = pd.concat([df_merged_stem, df_merged_neuron], axis=0)
-df_combined_mod=df_combined.drop(["condition","location","day"], axis=1)
+df_stem_separated=df_combined[df_combined["condition"]=="stem"]
+df_combined_mod=df_stem_separated.drop(["condition","location","day"], axis=1)
 df_combined_mod=df_combined_mod.reset_index(drop=True)
 df_combined_mod = df_combined_mod.set_index('index')
 df_combined_mod=df_combined_mod.T
 mean_expression=df_combined_mod.mean(axis=1)
-df_combined_mod["mean expression"]=mean_expression
+df_combined_mod["mean expression stem"]=mean_expression
 df_combined_mod=df_combined_mod.T
 df_combined.set_index("index", inplace=True)
 selected=df_combined[["condition", "location","day"]]
 df_final=pd.concat([df_combined_mod, selected], axis=1)
-print(df_final)
+df_final=df_final.dropna()
+df_final.loc["mean expression stem"]=mean_expression
 
 #separate the dataframes and calculate the means 
+df_combined = pd.concat([df_merged_stem, df_merged_neuron], axis=0)
+df_neuron_separated=df_combined[df_combined["condition"]=="neuron"]
+df_combined_n=df_neuron_separated.drop(["condition","location","day"], axis=1)
+df_combined_n=df_combined_n.reset_index(drop=True)
+df_combined_n = df_combined_n.set_index('index')
+df_combined_n=df_combined_n.T
+mean_expression_n=df_combined_n.mean(axis=1)
+df_combined_n["mean expression neuron"]=mean_expression_n
+df_combined_n=df_combined_n.T
+df_combined.set_index("index", inplace=True)
+selectedn=df_combined[["condition", "location","day"]]
+df_finaln=pd.concat([df_combined_n, selectedn], axis=1)
+df_finaln.loc["mean expression neuron"]=mean_expression_n
+df_finaln = df_finaln.drop(df_finaln.index[11:])
+selected_row_n = df_finaln.loc["mean expression neuron"]
+selected_row_s=df_final.loc["mean expression stem"]
+df_combined.loc["mean expression stem"]=selected_row_s
+df_combined.loc["mean expression neuron"]=selected_row_n
+print(df_combined)
+df_combined=df_combined.T
+
+
+def calculate_log2fc(row):
+    try:
+        # You can modify this to handle NaNs or infinite values if needed
+        return np.log2(row['mean expression neuron'] / row['mean expression stem'])
+    except ZeroDivisionError:
+        # Handle cases where division by zero happens
+        return np.nan
+
+# Apply the function to each row (axis=1 means row-wise)
+df_combined['log2FC'] = df_combined.apply(calculate_log2fc, axis=1)
+
+print(df_combined['mean expression neuron'].isna().sum())
+print(df_combined['mean expression stem'].isna().sum())
+df_combined = df_combined.dropna(subset=['mean expression neuron', 'mean expression stem'])
+
+def calculate_ttest(row):
+    # Extracting expression values for neurons and stem cells for the gene
+    neuron_values = row['mean expression neuron']
+    
+    # Perform t-test between neuron and stem cell expression
+    t_stat, p_value = stats.ttest_ind(neuron_values, stem_values)
+    return p_value
+
+# Apply the function to each row
+df_combined['p_value'] = df_combined.apply(calculate_ttest, axis=1)
+
+# Now df_combined will have a new column 'p_value' with the p-values of the t-tests
+print(df_combined)
